86c48bd2b8e2eecde58975ce9e8a44ec6cf96789
Kamil Marnik <ksdekamil@gmail.com>
2018-12-22 14:12:14 +0100

    Init project

---
+++ src/main/java/com/pwsz/project26_server/MillionairesApplication.java
@@ -0,0 +1,14 @@
+package com.pwsz.project26_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionairesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MillionairesApplication.class, args);
+    }
+
+}
+
