b6dc66de460a1523ba15f43e114b1b22ee5e76f8
Kamil Marnik <ksdekamil@gmail.com>
2018-12-22 14:13:27 +0100

    Init project

---
+++ src/test/java/com/pwsz/project26_server/MillionairesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pwsz.project26_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MillionairesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
